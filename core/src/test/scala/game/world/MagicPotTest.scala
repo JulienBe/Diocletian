a2f7f74fa77f8818ec8fe04e0818cacb449123bb
JulienBe <bertozzijulien@gmail.com>
2015-08-17 22:12:58 +0200

    Magic pot test

---
+++ core/src/test/scala/game/world/MagicPotTest.scala
@@ -0,0 +1,30 @@
+package game.world
+
+import game.objects.{Blocs, Thing}
+import org.scalatest.{Matchers, FlatSpec}
+import utilities.GdxMock
+
+import scala.collection.mutable
+
+/**
+ * Created by julien on 13/08/15.
+ */
+class MagicPotTest extends FlatSpec with Matchers {
+
+  it should "not place blocs outside boundries" in {
+    createWorld.forall(b => b.isInBoundary) should be(true)
+  }
+
+  it should "not make blocs overlap" in {
+    val blocs = createWorld
+    blocs.forall(
+        b => blocs.forall(other => other.overlap(b))
+    ) should be(false)
+  }
+
+  def createWorld = {
+    GdxMock.graphics
+    new MagicPot() create(new mutable.MutableList[Thing])
+  }
+
+}
